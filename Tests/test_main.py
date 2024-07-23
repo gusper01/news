e0e56de43a7803f950eee1ebed23b5b4cdfb9d06
gusper01 <gusper01@gmail.com>
2024-07-23 19:07:42 -0300

    tests

--- Tests/test_main.py
+++ Tests/test_main.py
@@ -1,8 +1,16 @@
-import pytest
-from src.main import get_news_from_rss
+import sys
+import os
+
+# Agregar la ruta del script al PYTHONPATH
+script_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../.github/workflows/scripts'))
+sys.path.insert(0, script_path)
+
+from fetch_news import get_news_from_rss
 
 def test_get_news_from_rss():
     url = 'https://www.reutersagency.com/feed/?best-topics=business-finance&post_type=best'
     df = get_news_from_rss(url)
-    assert df is not None
-    assert not df.empty
+    assert not df.empty  # Ejemplo de prueba simple
+
+if __name__ == "__main__":
+    test_get_news_from_rss()
